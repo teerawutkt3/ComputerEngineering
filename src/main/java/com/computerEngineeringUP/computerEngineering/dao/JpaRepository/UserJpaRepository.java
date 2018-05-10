23a6fbc5a60f87cba5271bfd3faf5175db536fc9
teerawut kulrittichai <teerawutkt3@gmail.com>
2018-05-10 10:49:58 +0700

    computer wengineering

--- src/main/java/com/computerEngineeringUP/computerEngineering/dao/JpaRepository/UserJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.computerEngineeringUP.computerEngineering.dao.JpaRepository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.computerEngineeringUP.computerEngineering.dao.entity.User;
-
-public interface UserJpaRepository extends CrudRepository<User, Integer>{
-
-}
